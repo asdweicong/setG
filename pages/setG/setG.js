82f4f8830d94994f8dd16b615633026b7bcc8261
weicon <1418066959@qq.com>
2020-03-26 22:06:44 +0800

    add

--- pages/setG/setG.js
+++ pages/setG/setG.js
@@ -9,8 +9,20 @@ Page({
     setGData:{
       a: 50,//单期数量
       b: 3,//叠加倍数
-      c: 3,//循环期数
+      c: {
+        value:3,
+        value_:0,
+        next:false,
+      },//循环期数
       d: 0,//总回报
+      e: {
+        value:'单',
+        bool:false
+      },//当前下注单双
+      f:  {
+        value:'双',
+        bool:false
+      },//当前下注大小
       _data: [],//每期数
     }
   },
@@ -70,11 +82,63 @@ Page({
   bindViewTapSetG: function () {
     console.log('bindViewTapSetG()');
     var data = this.data;
-    data.setGData._data.push(this.data.searchCode);
-    this.setData(data)
-    console.log(data)
-
+    var object = {
+      value:this.data.searchCode,
+      size:this.data.searchCode<5?'小':'大',
+      singleAndDouble:(this.data.searchCode%2==0)?'双':'单',
+      sizeWin:false,
+      singleAndDoubleWin:false,
+      win:false,
+    }
+    data.setGData._data.splice(0,0,object);
+    data.setGData = this.setNextValue(data.setGData);
+    this.setData(data);
     wx.setStorageSync('bindViewTapSetG', this.data.setGData)
+  },
+  setNextValue:function(setGData){
+    if(setGData.c.value_ == setGData.c.value){
+      setGData.c.value_ = 0;
+      setGData.e.bool = !setGData.e.bool;
+      setGData.f.bool = !setGData.f.bool;
+    }else{
+      setGData.c.value_++;
+    }
+    if(setGData.e.bool){
+      setGData.e.value = '双';
+    }else{
+      setGData.e.value = '单';
+    }
+    if(setGData.f.bool){
+      setGData.f.value = '小';
+    }else{
+      setGData.f.value = '大';
+    }
+    if(setGData._data[0].size == setGData.f.value ){
+      setGData._data[0].sizeWin = true
+    }
+    if(setGData._data[0].size == setGData.f.value ){
+      setGData._data[0].singleAndDoubleWin = true
+    }
+    if(setGData._data[0].sizeWin && setGData._data[0].singleAndDoubleWin){
+      setGData._data[0].win = true;
+    }
+    return setGData;
+  },
+  sizeFilter: function (value) {
+    console.log('sizeFilter',value)
+    if(value){
+      return '小'
+    }else{
+      return '大'
+    }
+  },
+  singleAndDoubleFilter: function (value) {
+    console.log('singleAndDoubleFilter',value)
+    if(value){
+      return '双'
+    }else{
+      return '单'
+    }
   },
   bindViewTapDeleteSetG: function () {
     console.log('bindViewTapDeleteSetG()');
