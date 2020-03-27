1d9d0fe807407554018bc68f9a4cc0ad88c2a375
职团项目组-凌伟聪 <weicong.ling@cignacmb.com>
2020-03-27 13:49:05 +0800

    add

--- pages/setG/setG.js
+++ pages/setG/setG.js
@@ -8,21 +8,27 @@ Page({
     searchCode:1,
     setGData:{
       a: 50,//单期数量
+      h:0,//下注数量
       b: 3,//叠加倍数
+      i:1,//叠加次数
+      j: 2,//循环个数
+      k: 2,//循环轮数
       c: {
-        value:3,
-        value_:0,
+        value:2,
+        value_:1,
+        valueDefault:1,
         next:false,
       },//循环期数
       d: 0,//总回报
       e: {
         value:'单',
-        bool:false
+        bool:false//false-->单  true-->双
       },//当前下注单双
       f:  {
-        value:'双',
-        bool:false
+        value:'小',
+        bool:false//false-->小  true-->大
       },//当前下注大小
+      g:false,//false -->大小  、true -->单双
       _data: [],//每期数
     }
   },
@@ -97,12 +103,13 @@ Page({
   },
   setNextValue:function(setGData){
     if(setGData.c.value_ == setGData.c.value){
-      setGData.c.value_ = 0;
+      setGData.c.value_ = setGData.c.valueDefault;
       setGData.e.bool = !setGData.e.bool;
       setGData.f.bool = !setGData.f.bool;
     }else{
       setGData.c.value_++;
     }
+    console.log(setGData.c.value_)
     if(setGData.e.bool){
       setGData.e.value = '双';
     }else{
@@ -113,15 +120,47 @@ Page({
     }else{
       setGData.f.value = '大';
     }
-    if(setGData._data[0].size == setGData.f.value ){
+    setGData._data[0].e = JSON.parse(JSON.stringify(setGData.e));
+    setGData._data[0].f = JSON.parse(JSON.stringify(setGData.f));
+    if(setGData._data[1].size == setGData.f.value ){
       setGData._data[0].sizeWin = true
     }
-    if(setGData._data[0].size == setGData.f.value ){
+    if(setGData._data[1].size == setGData.f.value ){
       setGData._data[0].singleAndDoubleWin = true
     }
-    if(setGData._data[0].sizeWin && setGData._data[0].singleAndDoubleWin){
-      setGData._data[0].win = true;
+    if(setGData._data.g){
+      if(setGData._data[0].sizeWin){
+        setGData.i = 0;
+        setGData._data[0].win = true;
+      }else{
+        setGData.i++;
+      }
     }
+    if(!setGData._data.g){
+      if(setGData._data[0].singleAndDoubleWin){
+        setGData.i = 0;
+        setGData._data[0].win = true;
+      }else{
+        setGData.i++;
+      }
+    }
+    
+    if(setGData._data[0].win){
+      setGData.d += setGData.h
+      setGData.h = setGData.a;
+      setGData.i = 0;
+    }else{
+      setGData.d -= setGData.h
+      if(setGData.h == 0){
+        setGData.h = setGData.a;
+      }
+      if(setGData.i >= (setGData.j * setGData.k)){
+        setGData.h = setGData.a;
+      }else{
+        setGData.h = setGData.h * setGData.b;
+      }
+    }
+    console.log(setGData)
     return setGData;
   },
   sizeFilter: function (value) {
